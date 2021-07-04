3174161381730b802b6914fdc5fd5404174931d9
dedrasher <78565796+dedrasher@users.noreply.github.com>
2021-07-04 15:14:32 +0300

    Add files via upload

---
+++ GayTest/GayTest/GayTest/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  GayTest
+//
+//  Created by Serega on 29.06.2021.
+//
+
+import Foundation
+struct Question {
+    var question: String
+    var questionImageSource: String
+    var yesAnswer = "Yes"
+    var noAnswer = "No"
+}
