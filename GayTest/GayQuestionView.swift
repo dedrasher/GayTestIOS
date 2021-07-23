fb3d4d9ae31710b5116624188aa3d04bcde2ff86
dedrasher <78565796+dedrasher@users.noreply.github.com>
2021-07-23 14:51:04 +0300

    Update GayQuestionView.swift

--- GayTest/GayQuestionView.swift
+++ GayTest/GayQuestionView.swift
@@ -7,14 +7,20 @@
 
 import SwiftUI
 struct GayQuestionView: View {
+    enum Answer{
+       case yes
+        case no
+        case unknown
+    }
     @Environment(\.presentationMode) var presentationMode
+    @State
+    var answers = [Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown,Answer.unknown]
     @State private var score = 0.0
     @State private var isGay = false
     @State private var blurRadius : CGFloat = 0.0
     @State private var result = ""
     @State private var showResult = false
     @State private var isHidden = false
-   @State private var questionIndex:Int = 0
  
     func loadHome() {
         DispatchQueue.main.async {
@@ -29,17 +35,24 @@ struct GayQuestionView: View {
             self.presentationMode.wrappedValue.dismiss()
                 }
     }
-    func increment(scoreIncrement: Bool) {
-        func addScore() {
-            if(scoreIncrement) {
-                score+=6.65
+    func answeredQuestionsCount() -> Int{
+        var count = 0
+        for answer in answers {
+            if answer != Answer.unknown {
+                count+=1
             }
         }
-        if(questionIndex < 14) {
-    questionIndex+=1
-            addScore()
-        } else {
-            addScore()
+        return count
+    }
+    func isAllQuestionsAnswered() -> Bool {
+        return !answers.contains(Answer.unknown)
+    }
+    func countResult() {
+        for answer in answers {
+            if answer == Answer.yes {
+                score += 6.65
+            }
+        }
             let isZero = score == 0.0
             if (isZero) {
                 result =    Test.name +  " (" + String(Test.age) + " years) " + "isn't gay"
@@ -50,7 +63,6 @@ struct GayQuestionView: View {
             score = 0.0
             self.blurRadius = 1.8
             showResult = true
-        }
     }
     private var questions: [Question] = [Question(question: "Do you play Brawl Stars?", questionImageSource:"brawl_stars"),
                         Question(question: "AMD or Intel?", questionImageSource:"intel_vs_amd", yesAnswer: "Intel", noAnswer: "AMD"),  Question(question: "Do you use cheats?", questionImageSource:"cheats"), Question(question: "Do you play fortnite?", questionImageSource:"fortnite"),
@@ -61,40 +73,51 @@ struct GayQuestionView: View {
                         
     ]
     var body: some View {
+        ScrollViewReader { view in
+        ScrollView {
+            ForEach(questions.indices) { i in
         VStack {
-            Text(questions[questionIndex].question).font(.system(size: 40)).scaledToFit().minimumScaleFactor(0.2).padding(.top, 20)
+            Text(questions[i].question).font(.system(size: 40)).scaledToFit().minimumScaleFactor(0.2).padding(.top, 20)
             Spacer()
-            Image(questions[questionIndex].questionImageSource).resizable().scaledToFit().padding(.horizontal, 10).clipShape(RoundedRectangle(cornerRadius: 45))
+            Image(questions[i].questionImageSource).resizable().scaledToFit().padding(.horizontal, 10).clipShape(RoundedRectangle(cornerRadius: 45))
 
             Spacer()
+            HStack {
             Button(action: {
-                increment(scoreIncrement: true)
+                answers[i] = Answer.yes
+                if isAllQuestionsAnswered() {
+                    countResult()
+                }
             }) {
-                Text(questions[questionIndex].yesAnswer)
+                Text(questions[i].yesAnswer)
                         .fontWeight(.semibold)
                         .font(.title)
                 .frame(minWidth: 0, maxWidth: .infinity)
                         .padding()
                 .foregroundColor(.black)
-                        .background(Color.blue)
-                        .cornerRadius(20).padding(.horizontal, 20).padding(.vertical, 5)
+                .background(answers[i] == Answer.yes  ? Color.gray : Color.blue)
+                        .cornerRadius(20).padding(.horizontal, 10).padding(.vertical, 5)
                 
             }
             Button(action: {
-                increment(scoreIncrement: false)
+                answers[i] = Answer.no
+                if isAllQuestionsAnswered() {
+                    countResult()
+                }
             }) {
-                Text(questions[questionIndex].noAnswer)
+                Text(questions[i].noAnswer)
                         .fontWeight(.semibold)
                         .font(.title)
                 .frame(minWidth: 0, maxWidth: .infinity)
                 .padding()
                 .foregroundColor(.black)
-                        .background(Color.blue)
-                        .cornerRadius(20).padding(.horizontal, 20).padding(.vertical, 5)
+                .background(answers[i] == Answer.no ? Color.gray : Color.blue)
+                        .cornerRadius(20).padding(.horizontal, 10).padding(.vertical, 5)
+            }
             }
         }.toolbar{
             ToolbarItem(placement: .principal){
-                Text("Question " +  String(questionIndex + 1) + " of 15").scaledToFill().font(.title2).blur(radius: blurRadius)
+                Text("Answered questions: " + String(answeredQuestionsCount()) + " of 15").scaledToFill().blur(radius: blurRadius)
 
             }
         }.alert(isPresented: $showResult) {
@@ -103,17 +126,23 @@ struct GayQuestionView: View {
                 self.loadHome()
             }, secondaryButton: .default(Text("Restart")){
                 self.blurRadius = 0.0
-                questionIndex = 0
-                score = 0.0
+                for i in answers.indices{
+                    answers[i] = Answer.unknown
+                }
+                withAnimation {
+                view.scrollTo(0)
+                }
             })
-        }.blur(radius: blurRadius)
-        
+        }.blur(radius: blurRadius).id(i)
+        }
+        }
+        }
         }
                                             
     
 }
 
-struct QuestionView_Previews: PreviewProvider {
+struct GayQuestionView_Previews: PreviewProvider {
     static var previews: some View {
         GayQuestionView()
     }
